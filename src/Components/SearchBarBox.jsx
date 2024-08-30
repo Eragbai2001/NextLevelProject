3740eae15e9babddc286ed002604533eb8cc7ae9
Josiah Aideloje <aideloje.josiah@lmu.edu.ng>
2024-08-30 15:05:28 +0100

    first commit

--- src/Components/SearchBarBox.jsx
+++ src/Components/SearchBarBox.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Search } from "lucide-react";
+import "../C";
 
 export const SearchBarBox = () => {
   return (
-    <div>
-      <Search />
+    <div className="input-wrapper">
+      <Search id="search-icon" />
       <input placeholder="Type to search..." />
     </div>
   );
