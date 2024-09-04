f1a42af585848d9cdf02c00d9d33813b8e3959ec
Josiah Aideloje <aideloje.josiah@lmu.edu.ng>
2024-09-04 17:03:14 +0100

    Recent Changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Next Level React/",
+  base: "/NextLevelReact/",
   plugins: [react()],
   root: "./",
   build: {
