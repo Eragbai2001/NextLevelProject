603951798e977f3e94c04b5bf1c53f1a87d6cd74
Josiah Aideloje <aideloje.josiah@lmu.edu.ng>
2024-09-04 14:41:27 +0100

    Recent Changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Specify your build output directory
+    outDir: "public", // Specify your build output directory
   },
 });
