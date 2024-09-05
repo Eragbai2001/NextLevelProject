674d088c071527c3b45bcd8e753a9c0cf75a8621
Josiah Aideloje <aideloje.josiah@lmu.edu.ng>
2024-09-05 10:32:17 +0100

    Recent Changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: "./", // Root directory for your project
-  build: {
-    outDir: "dist", // Output directory for the build
-  },
-  server: {
-    open: true, // Automatically open the app in the browser during local development
-  },
-});
+  server:{
+    port:3000,
+  }
+})
